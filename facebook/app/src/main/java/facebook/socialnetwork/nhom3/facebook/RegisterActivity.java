0c70420016d773401691106fb04056abc665f27d
ThanhNgan304 <43593243+ThanhNgan304@users.noreply.github.com>
2019-05-30 22:39:33 +0700

    thêm UI
    Profile

--- facebook/app/src/main/java/facebook/socialnetwork/nhom3/facebook/RegisterActivity.java
+++ facebook/app/src/main/java/facebook/socialnetwork/nhom3/facebook/RegisterActivity.java
@@ -2,12 +2,19 @@ package facebook.socialnetwork.nhom3.facebook;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
-
+    private EditText UserEmail, UserPassword, UserConfirmPassword;
+    private Button CreateAccountButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        UserEmail = (EditText) findViewById(R.id.editTextRegister_email);
+        UserPassword = (EditText) findViewById(R.id.editTextRegister_password);
+        UserConfirmPassword = (EditText) findViewById(R.id.editTextRegister_Confirm_password);
+        CreateAccountButton = (Button) findViewById(R.id.btnRegister_Create_account);
     }
 }
