570a06bf39e6b6168f462f4000743f1a82faa43f
kietnt98 <kietanhthongminh@gmail.com>
2019-05-31 16:39:50 +0700

    login
    login

--- facebook/app/src/main/java/facebook/socialnetwork/nhom3/facebook/LoginActivity.java
+++ facebook/app/src/main/java/facebook/socialnetwork/nhom3/facebook/LoginActivity.java
@@ -1,11 +1,21 @@
 package facebook.socialnetwork.nhom3.facebook;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -13,16 +23,23 @@ public class LoginActivity extends AppCompatActivity {
     private Button btnlogin, btnRegister;
     private EditText editTextUsername, editTextPassword;
 
+    private FirebaseAuth mAuth;
+    private ProgressDialog loadingBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
 
 
         editTextUsername = (EditText) findViewById(R.id.editTextLogin_email);
         editTextPassword = (EditText) findViewById(R.id.editTextLogin_password);
         btnlogin = (Button) findViewById(R.id.btLogin_login);
         btnRegister = (Button) findViewById(R.id.btLogin_Create_account);
+        loadingBar = new ProgressDialog(this);
+
 
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,6 +48,70 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AllowingUserToLogin();
+            }
+        });
+    }
+
+    private void AllowingUserToLogin() {
+        String email = editTextUsername.getText().toString();
+        String password = editTextPassword.getText().toString();
+
+        if (TextUtils.isEmpty(email)){
+
+            Toast.makeText(this, "Please write your email", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            if (TextUtils.isEmpty(password)){
+
+                Toast.makeText(this, "Please write your password", Toast.LENGTH_SHORT).show();
+            }
+            else{
+                loadingBar.setTitle("Login");
+                loadingBar.setMessage("Please wait, while we are allowing you to login into your account...");
+                loadingBar.show();
+                loadingBar.setCanceledOnTouchOutside(true);
+                mAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(task.isSuccessful()){
+                                    
+                                    SendUserToMainActivity();
+                                    Toast.makeText(LoginActivity.this, "You are logged in successfully.", Toast.LENGTH_SHORT).show();
+                                    loadingBar.dismiss();
+                                }
+                                else {
+                                    String message = task.getException().getMessage();
+                                    Toast.makeText(LoginActivity.this, "Error occured:"+message, Toast.LENGTH_SHORT).show();
+                                    loadingBar.dismiss();
+                                }
+                            }
+                        });
+            }
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        if(currentUser != null){
+            SendUserToMainActivity();
+            //SendUserToRegisterActivity();
+        }
+    }
+
+    private void SendUserToMainActivity() {
+        Intent mainIntent = new Intent(LoginActivity.this, MainActivity.class);
+        mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(mainIntent);
+        finish();
     }
 
 
--- facebook/app/src/main/java/facebook/socialnetwork/nhom3/facebook/MainActivity.java
+++ facebook/app/src/main/java/facebook/socialnetwork/nhom3/facebook/MainActivity.java
@@ -16,6 +16,11 @@ import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView postList;
     private Toolbar mToolbar;
     private FirebaseAuth mAuth;
+    private DatabaseReference UsersRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mAuth = FirebaseAuth.getInstance();
+        UsersRef = FirebaseDatabase.getInstance().getReference().child("Users");
 
         mToolbar = (Toolbar) findViewById(R.id.main_page_toolbar);
         setSupportActionBar(mToolbar);
@@ -67,8 +74,37 @@ public class MainActivity extends AppCompatActivity {
             SendUserToLoginActivity();
             //SendUserToRegisterActivity();
         }
+        else {
+            CheckUserExistence();
+        }
 
 
+    }
+
+    private void CheckUserExistence() {
+        final String current_user_id = mAuth.getCurrentUser().getUid();
+
+        UsersRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (!dataSnapshot.hasChild(current_user_id)){
+                   SendUserToSetupActivity();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+
+    private void SendUserToSetupActivity() {
+        Intent setupIntent = new Intent(MainActivity.this, SetupActivity.class);
+        setupIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(setupIntent);
+        finish();
     }
 
     private void SendUserToLoginActivity() {
